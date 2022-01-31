0b00e7f1e51954058992f4e008e948858b2ed909
Kevin Willer <asd8@protonmail.com>
2022-01-31 13:36:34 +0100

    optimize preprocessing

--- src/tweet-preprocessing/tweet-preprocessing.py
+++ src/tweet-preprocessing/tweet-preprocessing.py
@@ -1,85 +1,47 @@
 #!/usr/bin/env python3
-from collections import Counter
 import pandas as pd
 import nltk
 from nltk.corpus import stopwords
-import re,string
+import string
+import itertools
+from datetime import datetime
 
 #uncomment below if running for the first time
 #nltk.download('stopwords')
 
-csv_path   = "./data/twitter/raw/$ETH OR Ethereum until:2021-11-30-raw-2021-12-08 11:59:08.021502.csv"
-twt        = nltk.tokenize.TweetTokenizer(strip_handles=True, reduce_len=True)
-spamwords  = ("giveaway", "bot", "crypto") #wie kann ich die liste finessen?
-stop       = stopwords.words("english") + list(string.punctuation)
-df         = pd.read_csv(csv_path)
+def save_text_corpus(corpus, output_path, cashtag, timestamp):
+    # get aggregated tweets and save to txt file
+    filepath = output_path + f"{cashtag}_corpus_clean_{timestamp}.txt"
+    with open(filepath, "w+") as output:
+        output.write(str(list(itertools.chain(*corpus))))
 
-def spam_word(tweet, spamwords):
-    # drop row if tweet contains blacklisted term
-    if any(word in tweet for word in spamwords):
-        return True
-
-def spam_tags(tweet, n_tags=8):
-    # more than x tags in tweet should result in removal of tweet.
-    counter = Counter(tweet)
-    if counter["$"] >= n_tags or counter["#"] >= n_tags:
-        return True
-
-def drop_spam_row(tweet, index):
-    # drop row if spam_word or spam_tags indicate spam
-    if spam_word(tweet, spamwords) == True or spam_tags(tweet) == True:
-        df.drop(index, inplace=True)
-        return True
-
-def strip_numbers(tweet):
-    # many single numbers in the tweets. should delete
-    return re.sub(r'\d+', '', tweet)
-
-def strip_links(text):
-    # strip urls from tweets
-    link_regex    = re.compile('((https?):((//)|(\\\\))+([\w\d:#@%/;$()~_?\+-=\\\.&](#!)?)*)', re.DOTALL)
-    links         = re.findall(link_regex, text)
-    for link in links:
-        text = text.replace(link[0], ', ')
-    return text
-
-def strip_all_entities(text):
-    # strip @, #, $ tags from tweets
-    entity_prefixes = ['@','#','$']
-    for separator in  string.punctuation:
-        if separator not in entity_prefixes :
-            text = text.replace(separator,' ')
-    words = []
-    for word in text.split():
-        word = word.strip()
-        if word:
-            if word[0] not in entity_prefixes:
-                words.append(word)
-    return ' '.join(words)
-
-def preprocess_and_add_column(df):
-    # drop unwanted rows, preprocess tweet text and return a new dataframe
-    preprocessed = []
-    for index, row in df.iterrows():
-        tweet = row["TweetText"].lower()
-        spamrow = drop_spam_row(tweet, index)
-        if spamrow != True:
-            tweet = strip_numbers(tweet)
-            tweet = strip_links(tweet)
-            tweet = strip_all_entities(tweet)
-            tk_tweet = [token.lower() for token in twt.tokenize(tweet)
-                        if token.lower() not in stop]
-            if tk_tweet == []:
-                tk_tweet = "NaN"
-            preprocessed.append(tk_tweet)
-    df["PreprocessedTweetText"] = preprocessed
-    return df
-
-def save_to_csv(df, csv_path):
-    tmp_path = csv_path[:-4] + "-preprocessed" + csv_path[-4:]
+def save_to_csv(df, csv_path, timestamp):
+    tmp_path = csv_path[:-4] + f"-preprocessed_{timestamp}" + csv_path[-4:]
     out_path = tmp_path.replace("raw", "processed")
-    df.to_csv(out_path)
+    df.to_csv(out_path, index=False)
+
+def preprocess_and_add_column(df, cashtag, csv_path):
+    # drop unwanted rows, preprocess tweet text and return a new dataframe
+    df["text"] = df["text"].str.lower()
+    df = df[~df["text"].str.contains('|'.join(spamwords), na=False)]
+    df = df[(df["text"].str.count("#") <= 8) | (df["text"].str.count("$") <= 8) | (df["text"].str.count("@") <= 8)]
+    preprocessed = df['text'].replace(r'http\S+|\$\S+|\#\S+|\@\S+|[0-9.,$&:;?@#><*()%!-]|\n', '', regex=True)
+    preprocessed = preprocessed.apply(lambda tweet: twt.tokenize(tweet) if type(tweet) == str else "")
+    preprocessed = preprocessed.apply(lambda tweet: [word for word in tweet if word not in (stop)])
+    df["PreprocessedTweetText"] = preprocessed
+
+    timestamp = datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
+    corpus = save_text_corpus(preprocessed, "/home/kw/projects/crypto-sentiment/data/twitter/corpora/", cashtag, timestamp)
+    save_to_csv(df, csv_path, timestamp)
 
 if __name__ == "__main__":
-    preprocessed_df = preprocess_and_add_column(df)
-    save_to_csv(preprocessed_df, csv_path)
+
+    cashtag = "VANILLA"
+    csv_path   = f"./data/twitter/raw/{cashtag}_2021_09.csv"
+    twt        = nltk.tokenize.TweetTokenizer(strip_handles=True, reduce_len=True)
+    spamwords  = ("giveaway", "bot", "crypto", "whitelist") #wie kann ich die liste finessen?
+    stop       = stopwords.words("english") + list(string.punctuation)
+    df         = pd.read_csv(csv_path, encoding="utf-8", on_bad_lines="skip", engine="python")
+    #df_test    = pd.read_csv(csv_path).iloc[:20]
+
+    preprocess_and_add_column(df, cashtag, csv_path)
