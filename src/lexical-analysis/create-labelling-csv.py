ce244a3ec961c3835088defd3cc1146fdf28f37b
Kevin Willer <asd8@protonmail.com>
2022-02-03 15:18:09 +0100

    manual label csv sample script

--- src/lexical-analysis/create-labelling-csv.py
+++ src/lexical-analysis/create-labelling-csv.py
@@ -1,26 +1,47 @@
 #!/usr/bin/env python3
 import pandas as pd
 
-man  = "data/twitter/raw/manual_labelling_twitter.csv"
 
-avax = "data/twitter/raw/AVAX_from_2021-01-01T00:00:00Z_to_2021-12-31T23:59:59Z_api.csv"
-ftm  = "data/twitter/raw/FTM_from_2021-01-01T00:00:00Z_to_2021-12-31T23:59:59Z_api.csv"
-atom = "data/twitter/raw/ATOM_from_2021-01-01T00:00:00Z_to_2021-12-31T23:59:59Z_api.csv"
-sol  = "data/twitter/raw/SOL_from_2021-01-01T00:00:00Z_to_2021-12-31T23:59:59Z_api.csv"
+def man_label_sample(in_path, out_path, n_sample):
+    df = pd.read_csv(in_path)
+    df = df.sample(n_sample)
+    df = df[["text", "PreprocessedTweetText"]]
+    df.to_csv(out_path, index=False)
 
-df_avax     = pd.read_csv(avax)
-avax_sample = df_avax.sample(800)
+def sample_crypto_csv(ftm, avax, atom, sol, out, n_sample):
 
-df_ftm     = pd.read_csv(ftm)
-ftm_sample = df_ftm.sample(800)
+    df_avax     = pd.read_csv(avax)
+    avax_sample = df_avax.sample(n_sample)
+    print(avax_sample.shape)
 
-df_atom     = pd.read_csv(atom)
-atom_sample = df_atom.sample(800)
+    df_ftm     = pd.read_csv(ftm)
+    ftm_sample = df_ftm.sample(n_sample)
+    print(ftm_sample.shape)
 
-df_sol      = pd.read_csv(sol)
-sol_sample  = df_sol.sample(800)
+    df_atom     = pd.read_csv(atom)
+    atom_sample = df_atom.sample(n_sample)
+    print(atom_sample.shape)
 
-frames = [avax_sample, ftm_sample, atom_sample, sol_sample]
-result = pd.concat(frames)["text"]
+    df_sol      = pd.read_csv(sol)
+    sol_sample  = df_sol.sample(n_sample)
+    print(atom_sample.shape)
 
-result.to_csv(man, sep='\t')
+    frames = [avax_sample, ftm_sample, atom_sample, sol_sample]
+    result = pd.concat(frames)
+    print(result.shape)
+
+    result.to_csv(out, index=False)
+
+
+if __name__ == "__main__":
+
+    man_out  = "data/twitter/manual_labelling_twitter.csv"
+
+    avax_csv = "data/twitter/raw/AVAX_2021.csv"
+    ftm_csv  = "data/twitter/raw/FTM_2021.csv"
+    atom_csv = "data/twitter/raw/ATOM_2021.csv"
+    sol_csv  = "data/twitter/raw/SOL_2021.csv"
+    sample_csv = "data/twitter/processed/CRYPTO_2021_preprocessed.csv"
+
+    #sample_crypto_csv(ftm_csv, avax_csv, atom_csv, sol_csv, sample_csv, n_sample=250000)
+    man_label_sample(sample_csv, man_out, 10000)
