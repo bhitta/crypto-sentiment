022a86fcf29b51242d0a654aceb29bbd23816568
Kevin Willer <asd8@protonmail.com>
2022-03-22 15:10:07 +0100

    posneg -> vader score binning method

--- src/lexical_analysis/vader_scores.py
+++ src/lexical_analysis/vader_scores.py
@@ -11,12 +11,13 @@ import matplotlib.pyplot as plt
 from src.lexical_analysis.sa_eval import sentiment_score
 
 
-def score_dict_from_labels(label_path, freq_path, count_out_path):
+def score_dict_from_labels(label_path, freq_path, count_out_path, train_test_split):
     """
     takes csv with manual labels, file with common crypto words.
     outputs file with a dict of words and pos/neg occurences for manual cleanup.
     """
     labels = pd.read_csv(label_path)
+    labels = labels.iloc[: int(train_test_split * len(labels))]
     pos  = labels[labels["sentiment"] == 3.0]["PreprocessedTweetText"]
     neg  = labels[labels["sentiment"] == 1.0]["PreprocessedTweetText"]
     freq = pd.read_csv(freq_path).iloc[:, 0].tolist()
@@ -36,6 +37,13 @@ def score_dict_from_labels(label_path, freq_path, count_out_path):
         for key, value in fdict.items():
             writer.writerow([key, value])
 
+#def scale_up(x):
+#    #return x*(1 + save_max - x)
+#    return x * (1 + (max_val - x)/max_val)
+
+#def normalize_for_vader(x):
+#    return x  / max_val  * 4
+
 
 def clean_dict_to_vader_format(clean, neg, boost):
     score_df = pd.read_csv(clean, header=None)
@@ -43,26 +51,38 @@ def clean_dict_to_vader_format(clean, neg, boost):
     score_df.columns = ["token", "count"]
     score_df = score_df[score_df["token"].isin(neg) == False]
     score_df = score_df[score_df["token"].isin(boost) == False]
-    max_val = max(score_df["count"])
-    score_df["vader"] = score_df["count"].apply(lambda x: x / max_val * 4)
+    score_df_pos = score_df.loc[score_df["count"] > 15 ]
+    score_df_neg = score_df.loc[score_df["count"] < -1 ]
+    score_df =  pd.concat([score_df_pos, score_df_neg])
+    score_df["vader"] = 0
+    score_df["vader"][score_df["count"] >= 50] = 4
+    score_df["vader"][score_df["count"] <  50] = 3
+    score_df["vader"][score_df["count"] <  20] = 2
+    score_df["vader"][score_df["count"] <  -2] = -2
+    score_df["vader"][score_df["count"] <  -5] = -3
+    score_df["vader"][score_df["count"] <  -9] = -4
+    #max_val = max(score_df["count"])
+    #score_df["vader"] = score_df["count"].apply(lambda x: x * (1 + (max_val -x) / max_val))
+    #score_df["vader"] = score_df["count"].apply(lambda x: x * (1 + max_val - x))
+    #score_df["vader"] = score_df["vader"].apply(lambda x: x  / max_val  * 4)
     return score_df
 
 def plot_word_distribution(score_df, cut_off):
-    dist = score_df["vader"].to_list()
+    dist = score_df["count"].to_list()
     plt.hist(dist, bins=50)
-    plt.gca().set(title="Vader Score Distribution", ylabel="# of score", xlabel="VADER score")
-    plt.savefig("./data/posneg_words_vader.png")
+    plt.gca().set(title="PosNeg Score Distribution", ylabel="# of score", xlabel="PosNeg score")
+    plt.savefig("./data/posneg_words_posneg_score.png")
 
 def plot_sentiment_proportions(label_csv, preds):
+    labels = pd.read_csv(label_csv)
     if preds:
         labels["vanilla_pred"] = labels["text"].apply(lambda x: sentiment_score(x))
         pos  = len(labels[labels["vanilla_pred"] == 3.0])
         neg  = len(labels[labels["vanilla_pred"] == 1.0])
         neu  = len(labels[labels["vanilla_pred"] == 2.0])
-        title = "Predicted Tweets Sentiment Proportions"
-        out = "./data/labelled_sentiment_proportions.png"
+        title = "Predicted Tweets Sentiment Proportions with edited VADER Lexicon"
+        out = "./data/predicted_sentiment_proportions_edited.png"
     else:
-        labels = pd.read_csv(label_csv)
         pos  = len(labels[labels["sentiment"] == 3.0])
         neg  = len(labels[labels["sentiment"] == 1.0])
         neu  = len(labels[labels["sentiment"] == 2.0])
@@ -71,9 +91,9 @@ def plot_sentiment_proportions(label_csv, preds):
 
     total = sum([pos, neg, neu])
     sizes = [pos/total, neg/total , neu/total]
-    labels = ["Positive", "Negative", "Neutral"]
+    labels_arr = ["Positive", "Negative", "Neutral"]
     fig1, ax1 = plt.subplots()
-    ax1.pie(sizes,  labels=labels, autopct='%1.1f%%',
+    ax1.pie(sizes,  labels=labels_arr, autopct='%1.1f%%',
         shadow=True, startangle=90)
     ax1.axis('equal')
     plt.gca().set(title=title)
@@ -99,6 +119,7 @@ def adjust_vader_lexicon(lexicon_path, score, out_path):
 vanilla = "/home/kw/projects/crypto-sentiment/data/twitter/corpora/vanilla/VANILLA_corpus_clean_combined.txt"
 
 if __name__ == "__main__":
+    train_test_split = 0.8
     label_path =  "/home/kw/projects/crypto-sentiment/data/twitter/manual_labelling_twitter.csv"
     freq_path =  "/home/kw/projects/crypto-sentiment/data/twitter/high_freq/high_frequency_words_CRYPTO.csv"
     count_out_path = f"/home/kw/projects/crypto-sentiment/data/twitter/pos_neg_occurence{datetime.now()}.csv"
@@ -141,9 +162,9 @@ if __name__ == "__main__":
      "scarce" , "scarcely" , "slight" , "slightly" , "somewhat" ,
      "sort of" , "sorta" , "sortof" , "sort-of" }
 
-    #score_dict_from_labels(label_path, freq_path, count_out_path)
-    score = clean_dict_to_vader_format(clean, NEGATE, BOOSTER_DICT)
+    #score_dict_from_labels(label_path, freq_path, count_out_path, train_test_split)
+    #score = clean_dict_to_vader_format(clean, NEGATE, BOOSTER_DICT)
     #print(score.to_markdown())
     #plot_word_distribution(score, True)
-    adjust_vader_lexicon(vader_van, score, vader_out)
-    #plot_sentiment_proportions(label_path, preds=True)
+    #adjust_vader_lexicon(vader_van, score, vader_out)
+    plot_sentiment_proportions(label_path, preds=True)
